a642ca1f386ad07e1ff0746e1c90412936e75b6a
Nurken Kidirmaganbetov <nurkenkidir@gmail.com>
2023-03-21 13:38:43 +0600

    Added Lab5

---
+++ Lab5/Lab5/src/app/categories.comp/categories.comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories.comp',
+  templateUrl: './categories.comp.component.html',
+  styleUrls: ['./categories.comp.component.css']
+})
+export class CategoriesCompComponent {
+  
+}
